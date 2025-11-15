2c9b80db0ceff5713b26e75273b3810472a2f08d
Ivan Ivanyas <ivanyasivan1@gmail.com>
2025-11-15 19:21:59 +0300

    fix

--- server/routes/session.js
+++ server/routes/session.js
@@ -8,10 +8,7 @@ export default (app) => {
       const signInForm = {};
       reply.render('session/new', { signInForm });
     })
-    .post('/session', { name: 'session' }, app.fp.authenticate('form', {
-      successRedirect: '/',
-      failureRedirect: '/session/new',
-    }, async (req, reply, err, user) => {
+    .post('/session', { name: 'session' }, app.fp.authenticate('form', async (req, reply, err, user) => {
       if (err) {
         return app.httpErrors.internalServerError(err);
       }
@@ -26,7 +23,7 @@ export default (app) => {
       }
       await req.logIn(user);
       req.flash('success', i18next.t('flash.session.create.success'));
-      return reply.redirect(app.reverse('root'));
+      return reply.code(302).redirect(app.reverse('root'));
     }))
     .delete('/session', (req, reply) => {
       req.logOut();
