516a8c295289799469f7ce2700889b92b3c8d96f
Ivan Ivanyas <ivanyasivan1@gmail.com>
2025-11-15 18:56:12 +0300

    fix

--- server/locales/ru.js
+++ server/locales/ru.js
@@ -101,6 +101,8 @@ export default {
         new: {
           signIn: 'Вход',
           submit: 'Войти',
+          email: 'Email',
+          password: 'Пароль',
         },
       },
       users: {
